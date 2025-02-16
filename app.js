db7844ad25a4adfb3907f2645654cb7a68bf7e7d
conan <zknn@sina.com>
2025-02-16 13:52:36 +0800

    提交信息

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js
+App({
+  async onLaunch() {
+    // 使用callContainer前一定要init一下，全局执行一次即可
+    wx.cloud.init()
+    
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
