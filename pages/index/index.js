b4ae935ee56113e2bf881fcdf4f8f45040a43acf
conan <zknn@sina.com>
2025-02-23 19:42:36 +0800

    css modify

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,7 @@
-// index.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
 Page({
   data: {
-    motto: '欢迎使用星座运势',
     userInfo: {
       avatarUrl: defaultAvatarUrl,
       nickName: '',
@@ -12,11 +10,34 @@ Page({
     canIUseGetUserProfile: wx.canIUse('getUserProfile'),
     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
   },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../horos/index'
-    })
+
+  onLoad() {
+    // Initialize star positions
+    const stars = Array(12).fill(0).map(() => ({
+      left: Math.random() * 100 + '%',
+      top: Math.random() * 100 + '%',
+      animationDelay: Math.random() * 2 + 's'
+    }))
+    this.setData({ stars })
+
+    // Try to restore saved user info
+    const savedUserInfo = wx.getStorageSync('userInfo')
+    if (savedUserInfo && savedUserInfo.nickName && savedUserInfo.avatarUrl) {
+      this.setData({
+        userInfo: savedUserInfo,
+        hasUserInfo: true
+      })
+    }
   },
+
+  bindViewTap() {
+    if (this.data.hasUserInfo) {
+      wx.navigateTo({
+        url: '../horos/index'
+      })
+    }
+  },
+
   onChooseAvatar(e) {
     const { avatarUrl } = e.detail
     const { nickName } = this.data.userInfo
@@ -24,7 +45,9 @@ Page({
       "userInfo.avatarUrl": avatarUrl,
       hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
     })
+    this.saveUserInfo()
   },
+
   onInputChange(e) {
     const nickName = e.detail.value
     const { avatarUrl } = this.data.userInfo
@@ -32,16 +55,27 @@ Page({
       "userInfo.nickName": nickName,
       hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
     })
+    this.saveUserInfo()
+  },
+
+  saveUserInfo() {
     wx.setStorageSync('userInfo', this.data.userInfo)
   },
+
   getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      desc: '用于完善会员资料',
       success: (res) => {
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
+        })
+        this.saveUserInfo()
+      },
+      fail: (err) => {
+        wx.showToast({
+          title: '获取用户信息失败',
+          icon: 'none'
         })
       }
     })
